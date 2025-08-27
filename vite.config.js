a98197187c321bace7c574e08a3e00bfd28634ff
Maikel Suwisto <maikelsuwisto@gmail.com>
2025-08-27 12:18:49 +0700

    revise vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../build', // output build into backend/build
-  },
+  plugins: [
+    react(),
+    {
+      build: {
+        outDir: '../build', // output build into backend/build
+      },
+    }
+  ],
   preview: {
-    allowedHosts: ['.railway.app', 'web-production-3f9e.up.railway.app'],
+    allowedHosts: ['.railway.app'], // allow all Railway subdomains
     port: process.env.PORT || 4173,
-    host: true,
+    host: true
   },
   server: {
     port: process.env.PORT || 5173,
-    host: true,
+    host: true
   }
 })
